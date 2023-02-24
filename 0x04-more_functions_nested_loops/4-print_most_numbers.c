2ca970608b1bda94531fff4f8897c1ef9ea291c8
Abimbola Michael <abimbolamichael100@gmail.com>
2023-02-24 05:52:21 -0800

    Corrections

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,13 @@ void print_most_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i != 50 || i != 52)
-			_putchar(i);
+		if (i != 50)
+		{
+			if (i != 52)
+			{
+				_putchar(i);
+			}
+		}
 	}
 	_putchar('\n');
 }
