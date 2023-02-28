5344ecf235b8345449dcba559ffd1029793c97c8
Abimbola Michael <abimbolamichael100@gmail.com>
2023-02-28 10:23:02 -0800

    Correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,7 +20,7 @@ int _atoi(char *s)
 
 	while (i < length && f == 0)
 	{
-		if (s[i] == '_')
+		if (s[i] == '-')
 			++d;
 		if (s[i] >= '0' && s[i] <= '9')
 		{
@@ -29,7 +29,7 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > 9)
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
