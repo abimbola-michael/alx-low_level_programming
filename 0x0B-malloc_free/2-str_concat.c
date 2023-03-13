307cb65f61c750184fb3bb0686d660e177c369f5
Abimbola Michael <abimbolamichael100@gmail.com>
2023-03-13 08:17:01 -0700

    Correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,11 +27,17 @@ char *str_concat(char *s1, char *s2)
 	newstr = malloc(sizeof(char) * (i + j + 1));
 	if (newstr == NULL)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
+	while (s1[i] != '\0')
+	{
 		newstr[i] = s1[i];
-
-	for (j = 0; s2[j]; j++)
-		newstr[j] = s2[j];
-	newstr[j + i - 1] = '\0';
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		newstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	newstr[i] = '\0';
 	return (newstr);
 }
