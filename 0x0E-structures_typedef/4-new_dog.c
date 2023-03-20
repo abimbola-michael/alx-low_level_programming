dd6883fbe88235ecd52e92202a0d9c7b1b79932f
Abimbola Michael <abimbolamichael100@gmail.com>
2023-03-20 07:45:22 -0700

    Structures typedef tasks

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include <dog.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: input char
+ * @age: input float
+ * @owner: input char
+ * Return: dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int name_len, owner_len;
+
+	name_len = strlen(name);
+	owner_len = strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (name_len + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
