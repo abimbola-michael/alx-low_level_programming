45ca5e3f260381c602f13a047f0c3f01bf5cb563
Abimbola Michael <abimbolamichael100@gmail.com>
2023-03-20 07:55:22 -0700

    Correcction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,12 @@
  * Description: dog struct
  */
 
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
