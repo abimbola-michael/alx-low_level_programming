05e0d1038ec9ed5e56bd5377e25cc832511ce97b
Abimbola Michael <abimbolamichael100@gmail.com>
2023-04-03 10:42:28 -0700

    Task 9 and 10 Corrections

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = (*head)->next;
 		free(temp_head);
-		return (-1);
+		return (1);
 	}
 
 	while (i < index - 1)
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	temp_head = current->next;
 	free(current);
 
-	return (-1);
+	return (1);
 }
 
 
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 	listint_t *new_node;
 	listint_t *current;
 
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL || head == NULL)
 		return (NULL);
 
-	new_node->n = n;
+	new_node->n = n
 	new_node->next = NULL;
 
 	if (idx == 0)
